0fdc1e045f7232bb97b9533a099c996e92d96936
Gustavo Ygarzabal <ygarzabal.gustavo@gmail.com>
2023-11-23 20:58:38 -0300

    initial commit

---
+++ src/main/java/com/mym/point/PointApplication.java
@@ -0,0 +1,13 @@
+package com.mym.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mym/point/PointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mym.point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
